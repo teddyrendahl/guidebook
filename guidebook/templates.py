7f2d0335c05b30359f0dbf743c3dfb2be4a3a113
teddyrendahl <trendahl@slac.stanford.edu>
2017-08-20 21:10:40 -0700

    ENH: Added boulder template, don't show project grades

--- guidebook/templates.py
+++ guidebook/templates.py
@@ -8,7 +8,10 @@ problem=r"""
 % -------------------
 %  \VAR{name}  
 % -------------------
-\\VAR{color}{\VAR{number}}\hspace{1.5mm}\textbf{{\large \VAR{name}}} V\VAR{grade}
+\\VAR{color}{\VAR{number}}\hspace{1.5mm}\textbf{{\large \VAR{name}}}
+%- if color != 'project'
+V\VAR{grade}
+%- endif
 $ \color{red}
 %- for star in range(0, stars)
     \bigstar
@@ -19,3 +22,18 @@ $
 \newline
 \VAR{comment}
 \newline\newline"""
+
+###################################
+# Template for Individual Boulder #
+###################################
+boulder=r"""
+% ---------------------------------------------------
+% \VAR{name}
+% ---------------------------------------------------
+\section{\VAR{name}}
+%- if comment
+\VAR{comment}
+%- endif
+%- for problem in problems
+    \VAR{problem}
+%- endfor"""
