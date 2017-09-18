49c120453b6353f759b993a2f475ee14a387b713
teddyrendahl <trendahl@slac.stanford.edu>
2017-09-17 22:50:13 -0700

    MAINT: Circled problem takes color as a keyword

--- guidebook/templates.py
+++ guidebook/templates.py
@@ -8,8 +8,8 @@ problem=r"""
 % -------------------
 %  \VAR{name}  
 % -------------------
-\\VAR{color}{\VAR{number}}\hspace{1.5mm}\textbf{{\large \VAR{name}}}
-%- if color != 'project'
+\circlednumber{\VAR{number}}{\VAR{color}}\hspace{1.5mm}\textbf{{\large \VAR{name}}}
+%- if color != 'black'
 V\VAR{grade}
 %- endif
 $ \color{red}
@@ -37,3 +37,15 @@ boulder=r"""
 %- for problem in problems
     \VAR{problem}
 %- endfor"""
+
+##############################
+# Template for Rounded Table #
+##############################
+table=r"""
+\begin{rndtable}{l r r}}
+    \multicolumn{4}{c}{\cellcolor{color}\color{white} Highlights} \\ \hline
+%- for problem in problems
+    \VAR{problem.name} & \VAR{problem.grade} & %-for star in range(0, problem.stars)\bigstar%-endfor \\
+%- endfor
+    \end{rndtable}
+"""
